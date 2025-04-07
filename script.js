9a21ef99d32e50deff77b07428b4152a5ca29bd1
Aman Soni <amansoni@Amans-MacBook-Pro-2.local>
2025-04-07 16:48:53 +0530

    working on about section

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 var navbar = document.querySelector(".navbar");
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const gridCards = document.querySelectorAll('.grid-card');
+  
+    gridCards.forEach((card, index) => {
+      card.addEventListener('click', () => {
+        alert(`You clicked card #${index + 1}`);
+        card.classList.toggle('active-card');
+      });
+    });
+  });
+  
